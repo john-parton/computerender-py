6d128fbac84adfb97b235719f0aae29cefb03447
John Parton <john.parton.iv@gmail.com>
2022-10-10 12:52:50 -0500

    Style

--- src/computerender/sync.py
+++ src/computerender/sync.py
@@ -4,13 +4,15 @@ from decimal import Decimal
 from .api import Api
 
 
-# These methods are a repetitive 
+# These methods are a repetitive
+
 
 async def _generate_once(prompt, *args, **kwargs) -> bytes:
     api_key = kwargs.pop("api_key", None)
 
     async with Api(api_key=api_key) as api:
         return api.generate(prompt, *args, **kwargs)
+
 
 async def _cost_once(prompt, *args, **kwargs) -> Decimal:
     api_key = kwargs.pop("api_key", None)
@@ -21,8 +23,10 @@ async def _cost_once(prompt, *args, **kwargs) -> Decimal:
 
 # Sync wrappers
 
+
 def generate_sync(*args, **kwargs) -> bytes:
     return asyncio.run(_generate_once(*args, **kwargs))
+
 
 def cost_sync(*args, **kwargs) -> Decimal:
     return asyncio.run(_cost_once(*args, **kwargs))
